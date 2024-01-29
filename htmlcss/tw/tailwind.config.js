59981b0c4973f9498cdca342f043bb0d212a6fa2
안나영 <dpswpfskdud@naver.com>
2024-01-29 17:01:50 +0900

    tailwind 실습파일

--- htmlcss/tw/tailwind.config.js
+++ htmlcss/tw/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ocean' : '#16c0b0',
+        'leaf': '#84cf6a',
+        'mist': '#d8d8d8',
+        'midnight': '#39495c',
+      }
+    },
   },
   plugins: [],
 }
---
+++ js/hello.js
@@ -0,0 +1,10 @@
+var userName = 'nayoung'
+
+ 
+var age; //선언  declare +define  undefined 로 정의 된 것.
+let name; // undefined 
+console.log("🚀 ~ age:", age)
+age = 26;
+
+console.log("Hello,",userName);
+console.log(`Hello, ${userName}`);
